71faa6e22b7db7ff40cbe465e070a81d75f152df
Jay <lcm000522@gmail.com>
2026-01-19 00:41:58 +0900

    ADD : 09 - connecting ORM error check

--- src/Day9/index.ts
+++ src/Day9/index.ts
@@ -1,6 +1,13 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   // CREATE - 사용자 생성
